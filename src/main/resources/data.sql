bcf8b0511912d3b1d212df564efb3f13000fcefa
HackerBalistik <attrostephane@gmail.com>
2019-08-21 14:08:25 +0000

    Database Initialization with Spring

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO Category (description) VALUES ('American');
+INSERT INTO Category (description) VALUES ('Italian');
+INSERT INTO Category (description) VALUES ('Mexican');
+INSERT INTO Category (description) VALUES ('Fast Food');
+INSERT INTO unit_of_mesure (description) VALUES ('Teaspoon');
+INSERT INTO unit_of_mesure (description) VALUES ('Tablespoon');
+INSERT INTO unit_of_mesure (description) VALUES ('Cup');
+INSERT INTO unit_of_mesure (description) VALUES ('Pinch');
+INSERT INTO unit_of_mesure (description) VALUES ('Ounce');
